01ee2189bcc0f708902f2054f76b66da88779269
taekyang.lee <lty840422@naver.com>
2025-06-17 21:31:32 +0900

    a little further forward

--- README.md
+++ README.md
@@ -14,3 +14,5 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+1. 레이아웃 빌드: https://codelabs.developers.google.com/codelabs/flutter-animated-responsive-layout?hl=ko#3
--- lib/presentation/page/dribbble_design_01.dart
+++ lib/presentation/page/dribbble_design_01.dart
@@ -16,153 +16,193 @@ class _DribbbleDesign01State extends State<DribbbleDesign01> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue[800],
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+        ],
+      ),
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(25.0),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '안녕, 임꺽정!',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
+                              fontFamily: 'Pretendard',
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          SizedBox(height: 8.0),
+                          Text(
+                            '2025년 06월 15일',
+                            style: TextStyle(
+                              color: Colors.blue[200],
+                              fontSize: 12,
+                              fontFamily: 'Pretendard',
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.blue[600],
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        padding: EdgeInsets.all(12),
+                        child: Icon(Icons.notifications, color: Colors.white),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.blue[600],
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    padding: EdgeInsets.all(12),
+                    child: Row(
                       children: [
+                        Icon(Icons.search, color: Colors.white),
+                        SizedBox(width: 5),
                         Text(
-                          '안녕, 임꺽정!',
+                          '검색',
                           style: TextStyle(
                             color: Colors.white,
-                            fontSize: 24,
-                            fontFamily: 'Pretendard',
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                        SizedBox(height: 8.0),
-                        Text(
-                          '2025년 06월 15일',
-                          style: TextStyle(
-                            color: Colors.blue[200],
-                            fontSize: 12,
+                            fontSize: 14,
                             fontFamily: 'Pretendard',
                             fontWeight: FontWeight.w500,
                           ),
                         ),
                       ],
                     ),
-                    Container(
-                      decoration: BoxDecoration(
-                        color: Colors.blue[600],
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      padding: EdgeInsets.all(12),
-                      child: Icon(Icons.notifications, color: Colors.white),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 20),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.blue[600],
-                    borderRadius: BorderRadius.circular(12),
                   ),
-                  padding: EdgeInsets.all(12),
-                  child: Row(
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Icon(Icons.search, color: Colors.white),
-                      SizedBox(width: 5),
                       Text(
-                        '검색',
+                        '기분이 어때?',
                         style: TextStyle(
                           color: Colors.white,
-                          fontSize: 14,
+                          fontSize: 18,
                           fontFamily: 'Pretendard',
-                          fontWeight: FontWeight.w500,
+                          fontWeight: FontWeight.w400,
                         ),
+                      ),
+                      Icon(Icons.more_horiz, color: Colors.white),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        children: [
+                          EmoticonFace(emoticonFace: '🦀'),
+                          SizedBox(height: 8),
+                          Text(
+                            '나쁨',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'Pretendard',
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          EmoticonFace(emoticonFace: '✅'),
+                          SizedBox(height: 8),
+                          Text(
+                            '좋음',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'Pretendard',
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          EmoticonFace(emoticonFace: '💡'),
+                          SizedBox(height: 8),
+                          Text(
+                            '울음',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'Pretendard',
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          EmoticonFace(emoticonFace: '✨'),
+                          SizedBox(height: 8),
+                          Text(
+                            '짜증',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'Pretendard',
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 25),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ClipRRect(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(25),
+                  topRight: Radius.circular(25),
+                ),
+                child: Container(
+                  padding: EdgeInsets.all(20),
+                  color: Colors.white,
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            '운동',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontFamily: 'Pretendard',
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          Icon(Icons.more_horiz),
+                        ],
                       ),
                     ],
                   ),
                 ),
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      '기분이 어때?',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontFamily: 'Pretendard',
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                    Icon(Icons.more_horiz, color: Colors.white),
-                  ],
-                ),
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Column(
-                      children: [
-                        EmoticonFace(emoticonFace: '🦀'),
-                        SizedBox(height: 8),
-                        Text(
-                          '나쁨',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontFamily: 'Pretendard',
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        EmoticonFace(emoticonFace: '✅'),
-                        SizedBox(height: 8),
-                        Text(
-                          '좋음',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontFamily: 'Pretendard',
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        EmoticonFace(emoticonFace: '💡'),
-                        SizedBox(height: 8),
-                        Text(
-                          '울음',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontFamily: 'Pretendard',
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        EmoticonFace(emoticonFace: '✨'),
-                        SizedBox(height: 8),
-                        Text(
-                          '짜증',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontFamily: 'Pretendard',
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ],
+              ),
             ),
-          ),
+          ],
         ),
       ),
     );
